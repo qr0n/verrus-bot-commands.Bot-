808294cb4104a3a50da776d60bc9945f477cca23
bothost <77075118+qr0n@users.noreply.github.com>
2021-03-17 13:28:03 +0000

    logger on slash command hexe

--- main.py
+++ main.py
@@ -843,6 +843,7 @@ async def hexe(ctx, *, arg):
   if arg == "pip install":
     await ctx.send("installed package", hidden=True)
   await ctx.send(f"{a}", hidden=True)
+  print(f"{author.name}, {arg}")
 
 keep_alive()
 bot.run(os.environ.get('OEN'))
